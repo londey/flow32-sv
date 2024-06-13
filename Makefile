cbae265e3f50a01c9b00d711f178f562a15195eb
Nicholas Londey <londey@gmail.com>
2024-06-13 10:25:59 +0000

    work in progress

--- Makefile
+++ Makefile
@@ -4,7 +4,9 @@ TEST_BENCHES := $(wildcard test/testbench/*.sv)
 
 TOP = Top
 
+
 all:
+	@make clean
 	@make synth
 	@make place
 	@make pack
@@ -15,23 +17,27 @@ all:
 srcs:
 	@echo $(SRCS)
 
+
 synth: $(SRCS)
 	@echo $(SRCS)
 	@mkdir -p build
 	@yosys -p 'synth_ice40 -abc9 -top $(TOP) -json build/design.json' $(SRCS)
 
+
 place: build/design.json
-	@rm build/design.asc
+	@rm -f build/design.asc
 	@nextpnr-ice40 --Werror --package sg48 --up5k --freq 48 --top Top --json build/design.json --pcf src/pinmap.pcf --asc build/design.asc
-# @arachne-pnr -d 1k -P tq144:4k -p build/pinmap.pcf build/design.blif -o build/design.txt
+
 
 pack: build/design.asc
-	@rm build/design.bin
+	@rm -f build/design.bin
 	@icepack build/design.asc build/design.bin
 
+
 uf2: build/design.bin
-	@rm build/design.uf2
+	@rm -f build/design.uf2
 	@bin2uf2 -o build/design.uf2 build/design.bin
+
 
 verilator: $(SRCS)
 	@mkdir -p build/verilator/obj
@@ -39,12 +45,10 @@ verilator: $(SRCS)
 	@make -C build/verilator -j -f build/verilator/obj/VTop.mk VTop
 	@./build/verilator/VTop
 
+
 clean:
-	@rm -f build/*
- 
-#  The  $(SRCS)  variable is a list of all the .vs files in the src directory. The  all  target is the default target that will be run when you type  make  in the terminal. It runs the  yosys  command to synthesize the design, then the  arachne-pnr  command to place and route the design, and finally the  icepack  command to generate the binary file. The  clean  target is used to remove all the files in the build directory. 
-#  The  @  symbol at the beginning of each command suppresses the output of the command. 
-#  The  $(SRCS)  variable is used to pass the list of .vs files to the  yosys  command.
+	@rm -rf build/*
+
 
 build/test/%.vvp: test/%.sv
 	@mkdir -p build/test
--- src/Top.sv
+++ src/Top.sv
@@ -1,21 +1,13 @@
 
-// Note: Comment out this line when building in iCEcube2:
-//`include "Sync_To_Count.v"
-
 
 module Top 
- #(//parameter SUB_PIXEL_WIDTH = 3,
+ #(
 )(
-    input wire clock,
-    input wire i_reset_n,
-    // input UART_RX,
-    // output UART_TX
+    input wire CLK,
+    input wire ICE_PB,
     output reg LED_R,
     output reg LED_G,
     output reg LED_B
-
-    // input UART_RX,
-    // output UART_TX
 );
 
     localparam N = 24;
@@ -24,9 +16,9 @@ module Top
     reg [1:0] led;
 
     
-
-    always_ff @(posedge clock) begin
-        if (!i_reset_n) begin
+ 
+    always_ff @(posedge CLK) begin
+        if (!ICE_PB) begin
             led <= 0;
             counter <= 1;
         end else if (counter == 0) begin
@@ -35,13 +27,6 @@ module Top
         end else begin
             counter <= counter + 1;
         end
-        // if (i_reset_n) begin
-        //     counter <= 0;
-        // end else if (counter == (N ** 2) - 1) begin
-        //     counter <= 0;
-        // end else begin
-        //     counter <= counter + 1;
-        // end
 
         counter <= counter + 1;
     end
@@ -49,46 +34,5 @@ module Top
     assign LED_R = !(led == 2'b00);
     assign LED_G = !(led == 2'b01);
     assign LED_B = !(led == 2'b10);
-
-    // assign UART_TX = UART_RX;
-
-
-    // assign UART_TX = UART_RX;
-  // wire w_VSync;
-  // wire w_HSync;
-  
-  
-  // // Patterns have 16 indexes (0 to 15) and can be g_Video_Width bits wide
-  // wire [SUB_PIXEL_WIDTH-1:0] Pattern_Red[0:15];
-  // wire [SUB_PIXEL_WIDTH-1:0] Pattern_Grn[0:15];
-  // wire [SUB_PIXEL_WIDTH-1:0] Pattern_Blu[0:15];
-  
-  // // Make these unsigned counters (always positive)
-  // wire [9:0] w_Col_Count;
-  // wire [9:0] w_Row_Count;
-
-  // wire [6:0] w_Bar_Width;
-  // wire [2:0] w_Bar_Select;
-  
-  // Sync_To_Count #(.TOTAL_COLS(TOTAL_COLS),
-  //                 .TOTAL_ROWS(TOTAL_ROWS))
-  
-  // UUT (.i_Clk      (i_Clk),
-  //      .i_HSync    (i_HSync),
-  //      .i_VSync    (i_VSync),
-  //      .o_HSync    (w_HSync),
-  //      .o_VSync    (w_VSync),
-  //      .o_Col_Count(w_Col_Count),
-  //      .o_Row_Count(w_Row_Count)
-  //     );
-	  
-  
-  // // Register syncs to align with output data.
-  // always_ff @(posedge i_Clk)
-  // begin
-  //   o_vsync <= w_vsync;
-  //   o_hsync <= w_hsync;
-  // end
-  
 
 endmodule
