36709e9a776cf44ca4b4bddce2e7b28be66abc6d
Nicholas Londey <londey@gmail.com>
2024-06-13 21:39:14 +1000

    start of UART echo

--- src/Top.sv
+++ src/Top.sv
@@ -7,32 +7,19 @@ module Top
     input wire ICE_PB,
     output reg LED_R,
     output reg LED_G,
-    output reg LED_B
+    output reg LED_B,
+    output wire UART_TX,
+    input wire UART_RX
 );
 
-    localparam N = 24;
-
-    reg [N-1:0] counter;
-    reg [1:0] led;
-
-    
- 
     always_ff @(posedge CLK) begin
-        if (!ICE_PB) begin
-            led <= 0;
-            counter <= 1;
-        end else if (counter == 0) begin
-            led <= led + 1;
-            counter <= counter + 1;
-        end else begin
-            counter <= counter + 1;
-        end
 
-        counter <= counter + 1;
     end
 
-    assign LED_R = !(led == 2'b00);
-    assign LED_G = !(led == 2'b01);
-    assign LED_B = !(led == 2'b10);
+    assign LED_R = UART_RX;
+    assign LED_G = !UART_RX;
+    assign LED_B = ICE_PB;
+
+    assign UART_TX = UART_RX;
 
 endmodule
